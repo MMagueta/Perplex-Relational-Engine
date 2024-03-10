0894a86476a69a040684dedb0db2500e56f3baed
Victor Freire <25647735+ratsclub@users.noreply.github.com>
2024-03-10 18:52:00 -0300

    add docker image (#5)

--- src/Language/Language.fs
+++ src/Language/Language.fs
@@ -52,8 +52,8 @@ module Value = begin
             | VInteger32 v ->
                 ("VInteger32", v)
             | VVariableString v ->
-                ("VVariableString", v)
-         static Deserialize((name, value): string * obj): t =
+                ( "VVariableString", v)
+         static member Deserialize((name, value): string * obj): t =
             match name with
             | "VInteger32" ->
                 VInteger32 (value :?> int32)
